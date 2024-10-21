d08c1805fe79046981914cdbe2bc5215d5180362
Sara Saiz <ssl163@alumnos.unican.es>
2024-10-21 17:48:46 +0200

    Error database corregido

--- AndroidProject/app/src/main/java/es/unican/gasolineras/repository/DatabaseFunction.java
+++ AndroidProject/app/src/main/java/es/unican/gasolineras/repository/DatabaseFunction.java
@@ -9,13 +9,13 @@ public class DatabaseFunction {
     private static AppDatabase database;
 
     public static AppDatabase getDatabase(Context context) {
-
-        // Inicializa la base de datos si aún no lo está
-        database = Room.databaseBuilder(context.getApplicationContext(),
-                        AppDatabase.class, "database-name")
-                .fallbackToDestructiveMigration()  // Opcional: elimina la base de datos si hay un cambio de esquema.
-                .build();
-
+        if (database == null) {
+            // Inicializa la base de datos si aún no lo está
+            database = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, "database-name")
+                    .fallbackToDestructiveMigration()  // Opcional: elimina la base de datos si hay un cambio de esquema.
+                    .build();
+        }
         return database;
     }
 }
