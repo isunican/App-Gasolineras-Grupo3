25f2d45ee97c672cf10a4e8b59cbb38d39a290fc
alexiasd678 <asd678@alumnos.unican.es>
2024-10-21 17:58:06 +0200

    Inicio implementacion plan de pruebas, existen errores

---
+++ AndroidProject/app/src/test/java/es/unican/gasolineras/ConsultarRepostaje/ConsultarRepostajeTest.java
@@ -0,0 +1,21 @@
+package es.unican.gasolineras.ConsultarRepostaje;
+
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+//@RunWith(RobolectricTestRunner.class)
+public class ConsultarRepostajeTest {
+
+    @Test
+    public void calcularAcumuladoMesTest(){
+        //Context context = ApplicationProvider.getApplicationContext();
+        assertEquals()
+
+
+    }
+}
