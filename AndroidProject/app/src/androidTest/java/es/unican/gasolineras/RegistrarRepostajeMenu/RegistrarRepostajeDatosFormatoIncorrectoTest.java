e3edcc80428eac8323a6c572ac61f17b1105150b
Gustavo Gonzalez <ggf154@alumnos.unican.es>
2024-10-23 16:55:34 +0200

    Creacion del test de interfaz de datos con formato incorrecto

---
+++ AndroidProject/app/src/androidTest/java/es/unican/gasolineras/RegistrarRepostajeMenu/RegistrarRepostajeDatosFormatoIncorrectoTest.java
@@ -0,0 +1,70 @@
+package es.unican.gasolineras.RegistrarRepostajeMenu;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static es.unican.gasolineras.utils.Matchers.withBackgroundColor;
+import static es.unican.gasolineras.utils.MockRepositories.getTestRepository;
+
+import android.content.Context;
+import android.graphics.Color;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import dagger.hilt.android.testing.BindValue;
+import dagger.hilt.android.testing.HiltAndroidRule;
+import dagger.hilt.android.testing.HiltAndroidTest;
+import dagger.hilt.android.testing.UninstallModules;
+import es.unican.gasolineras.R;
+import es.unican.gasolineras.activities.RegistrarRepostajeMenu.RegistrarView;
+import es.unican.gasolineras.injection.RepositoriesModule;
+import es.unican.gasolineras.repository.IGasolinerasRepository;
+
+@UninstallModules(RepositoriesModule.class)
+@HiltAndroidTest
+@RunWith(AndroidJUnit4.class)
+public class RegistrarRepostajeDatosFormatoIncorrectoTest {
+
+    @Rule(order = 0)  // the Hilt rule must execute first
+    public HiltAndroidRule hiltRule = new HiltAndroidRule(this);
+
+    @Rule(order = 1)
+    public ActivityScenarioRule<RegistrarView> activityRule = new ActivityScenarioRule<>(RegistrarView.class);
+
+    // I need the context to access resources, such as the json with test gas stations
+    final Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+    // Mock repository that provides data from a JSON file instead of downloading it from the internet.
+    @BindValue
+    final IGasolinerasRepository repository = getTestRepository(context, R.raw.gasolineras_ccaa_06);
+
+    @Test
+    public void datosFormatoIncorrectoContext() {
+        onView(withId(R.id.textLitros)).perform(click());
+        onView(withId(R.id.textLitros)).perform(typeText("Veinte"));
+        Espresso.pressBackUnconditionally();
+        onView(withId(R.id.textPrecioTotal)).perform(click());
+        onView(withId(R.id.textPrecioTotal)).perform(typeText("20"));
+        Espresso.pressBackUnconditionally();
+
+
+        onView(withId(R.id.btnGuardar)).perform(click());
+
+        onView(withId(R.id.tvError)).check(matches(withText("Error: Los datos introducidos no tienen el formato correcto")));
+
+        onView(withId(R.id.textLitros)).check(matches(withBackgroundColor(Color.RED)));
+
+
+    }
+
+}
--- AndroidProject/app/src/androidTest/java/es/unican/gasolineras/utils/Matchers.java
+++ AndroidProject/app/src/androidTest/java/es/unican/gasolineras/utils/Matchers.java
@@ -1,7 +1,10 @@
 package es.unican.gasolineras.utils;
 
+import android.graphics.drawable.ColorDrawable;
 import android.view.View;
 import android.widget.ListView;
+
+import androidx.test.espresso.matcher.BoundedMatcher;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -12,6 +15,19 @@ import org.hamcrest.TypeSafeMatcher;
  */
 public class Matchers {
 
+    public static Matcher<View> withBackgroundColor(final int expectedColor) {
+        return new BoundedMatcher<View, View>(View.class) {
+
+            public void describeTo(Description description) {
+                description.appendText("with background color: ");
+            }
+
+            public boolean matchesSafely(View view) {
+                int currentColor = ((ColorDrawable) view.getBackground()).getColor();
+                return currentColor == expectedColor;
+            }
+        };
+    }
 
 
 }
