0222c429abc0bd1a885ad76ee126e1b6ad121442
alexiasd678 <asd678@alumnos.unican.es>
2024-11-02 13:30:44 +0100

    Solucionados errores actualizacion fechas test sprint01 en feature/506087

--- AndroidProject/app/src/test/java/es/unican/gasolineras/ConsultarRepostaje/ConsultarRepostajeTest.java
+++ AndroidProject/app/src/test/java/es/unican/gasolineras/ConsultarRepostaje/ConsultarRepostajeTest.java
@@ -1,11 +1,6 @@
 package es.unican.gasolineras.ConsultarRepostaje;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import android.content.Context;
@@ -19,6 +14,10 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
 import java.util.List;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 
 import es.unican.gasolineras.activities.ConsultarRepostaje.ConsultarPresenter;
 import es.unican.gasolineras.model.Repostaje;
@@ -35,8 +34,14 @@ public class ConsultarRepostajeTest {
     private RepostajeDAO mockRepostajeDAO;
 
     private ConsultarPresenter consultarPresenter;
-    private String fechaIni = "2024-10-01";
-    private String fechaFin = "2024-10-31";
+
+    //Obtenemos el mes y el año actual
+    YearMonth mesActual = YearMonth.now();
+    LocalDate fechaIni = mesActual.atDay(1);
+    LocalDate fechaFin = mesActual.atEndOfMonth();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    String fechaIniStr = fechaIni.format(formatter);
+    String fechaFinStr = fechaFin.format(formatter);
 
     @Before
     public void inicializa() throws Exception {
@@ -62,20 +67,20 @@ public class ConsultarRepostajeTest {
         Context context = ApplicationProvider.getApplicationContext();
 
         //repostajesMes contiene una lista vacía ([]).
-        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIni, fechaFin))
+        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIniStr, fechaFinStr))
                 .thenReturn(List.of());
         res0 = consultarPresenter.calcularAcumuladoMes();
         assertEquals(0.0, res0);
 
         //repostajesMes contiene una lista con un objeto Repostaje(precioTotal = 30.0).
-        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIni, fechaFin))
+        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIniStr, fechaFinStr))
                 .thenReturn(List.of(repostaje1));
         res1 = consultarPresenter.calcularAcumuladoMes();
         assertEquals(30.0, res1);
 
         //repostajesMes contiene una lista con dos objetos de tipo Repostaje:
         //Repostaje(precioTotal = 30.0), Repostaje(precioTotal = 12.34).
-        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIni, fechaFin))
+        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIniStr, fechaFinStr))
                 .thenReturn(List.of(repostaje1, repostaje2));
         res2 = consultarPresenter.calcularAcumuladoMes();
         assertEquals(42.34, res2);
@@ -83,7 +88,7 @@ public class ConsultarRepostajeTest {
 
         //repostajesMes contiene una lista con tres objetos de tipo Repostaje:
         //Repostaje(precioTotal = 30.0), Repostaje(precioTotal = 12.34), Repostaje(precioTotal = 56.78).
-        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIni, fechaFin))
+        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIniStr, fechaFinStr))
                 .thenReturn(List.of(repostaje1, repostaje2, repostaje3));
         res3 = consultarPresenter.calcularAcumuladoMes();
         assertEquals(99.12, res3);
@@ -96,21 +101,21 @@ public class ConsultarRepostajeTest {
         Context context = ApplicationProvider.getApplicationContext();
 
         //repostajesMesAnterior contiene una lista vacía ([]).
-        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIni, fechaFin))
+        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIniStr, fechaFinStr))
                 .thenReturn(List.of());
         res0 = consultarPresenter.calcularPrecioMedioLitro();
         assertEquals(0.0, res0);
 
         //repostajesMesAnterior contiene una lista con un objeto
         //Repostaje(precioTotal = 30.0, litros = 25.40).
-        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIni, fechaFin))
+        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIniStr, fechaFinStr))
                 .thenReturn(List.of(repostaje1));
         res1 = consultarPresenter.calcularPrecioMedioLitro();
         assertEquals(1.18, res1, 0.01);
 
         //repostajesMesAnterior contiene una lista con dos objetos de tipo Repostaje:
         //Repostaje(precioTotal = 30.0, litros = 25.40), Repostaje(precioTotal = 12.34, litros = 7.86).
-        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIni, fechaFin))
+        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIniStr, fechaFinStr))
                 .thenReturn(List.of(repostaje1, repostaje2));
         res2 = consultarPresenter.calcularPrecioMedioLitro();
         assertEquals(1.27, res2, 0.01);
@@ -118,7 +123,7 @@ public class ConsultarRepostajeTest {
         //repostajesMesAnterior contiene una lista con tres objetos de tipo Repostaje:
         //Repostaje(precioTotal = 30.0, litros = 25.40), Repostaje(precioTotal = 12.34, litros = 7.86),
         //Repostaje(precioTotal = 56.78, litros = 47.33).
-        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIni, fechaFin))
+        when(mockRepostajeDAO.repostajesPorRangoDeFechas(fechaIniStr, fechaFinStr))
                 .thenReturn(List.of(repostaje1, repostaje2, repostaje3));
         res3 = consultarPresenter.calcularPrecioMedioLitro();
         assertEquals(1.22, res3, 0.01);
