a84af5dbf243810f210d1585ee5585a837b63cd9
alexiasd678 <asd678@alumnos.unican.es>
2024-11-03 19:03:25 +0100

    Sigo implementando codigo, aparecen errores a la hora de guardar los datos

--- AndroidProject/app/src/main/java/es/unican/gasolineras/activities/RegistrarDescuentoEnMarca/RegistrarDescuentoPresenter.java
+++ AndroidProject/app/src/main/java/es/unican/gasolineras/activities/RegistrarDescuentoEnMarca/RegistrarDescuentoPresenter.java
@@ -1,12 +1,7 @@
 package es.unican.gasolineras.activities.RegistrarDescuentoEnMarca;
 
-import java.util.List;
-
-import es.unican.gasolineras.activities.ConsultarRepostaje.IConsultar;
-import es.unican.gasolineras.activities.RegistrarRepostajeMenu.IRegistrar;
-import es.unican.gasolineras.model.Gasolinera;
+import es.unican.gasolineras.model.Descuento;
 import es.unican.gasolineras.repository.DescuentoDAO;
-import es.unican.gasolineras.repository.RepostajeDAO;
 
 public class RegistrarDescuentoPresenter implements IRegistrarDescuento.Presenter{
 
@@ -18,35 +13,61 @@ public class RegistrarDescuentoPresenter implements IRegistrarDescuento.Presente
     }
 
     /**
-     * @see IRegistrarDescuento.Presenter#init(IResgistarDescuento.View)
+     * @see IRegistrarDescuento.Presenter#init(IRegistrarDescuento.View)
      * @param view the view to control
      */
     @Override
     public void init(IRegistrarDescuento.View view){
-
         this.view = view;
         this.view.init();
-        load();
-
-    }
-
-    private void load(){
-
-    }
-
-
-    /**
-     * @see IRegistrar.Presenter#onBtnGuardarClicked(String, String)
-     * @param marca marca a la que aplicar el filtro
-     * @param descuento descuento que se aplica
-     */
-    public void onBtnGuardarClicked(String marca, int descuento) {
     }
 
     /**
-     * @see IRegistrar.Presenter#onBtnCancelarClicked()
+     * @see IRegistrarDescuento.Presenter#onBtnCancelarClicked()
      */
     public void onBtnCancelarClicked() {
         view.showBtnCancelar();
+    }
+
+    /**
+     * @see IRegistrarDescuento.Presenter#onBtnGuardarClicked(String, Integer)
+     * @param marca marca a la que aplicar el filtro
+     * @param descuento descuento que se aplica
+     */
+    public void onBtnGuardarClicked(String marca, Integer descuento) {
+        boolean errorDescuento;
+
+        if (descuento == null){
+            errorDescuento = true;
+            view.mostrarError("Error: El campo no debe estar vacio", errorDescuento);
+            return;
+        }
+
+        //Validar que el valor sea positivo
+        if(descuento < 0){
+            errorDescuento = true;
+            view.mostrarError("Error: El valor debe ser positivo", errorDescuento);
+            return;
+        }
+
+        //Validar que el valor sea menor o igual a 0
+        if(descuento > 100){
+            errorDescuento = true;
+            view.mostrarError("Error: El valor debe ser menor que 100", errorDescuento);
+            return;
+        }
+
+        //Crear el objeto descuento
+        Descuento desc = new Descuento();
+        desc.setMarca(marca);
+        desc.setDescuento(descuento);
+
+        try {
+            descuentoDAO.registrarDescuento(desc);
+            view.showBtnGuardar(marca, descuento);
+        }catch (Exception e){
+            view.mostrarError("Error al registrar el descuento en la base de datos", false);
+        }
+
     }
 }
