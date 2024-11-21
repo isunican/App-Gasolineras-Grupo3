68dd240f7c13c08939999a0ebffc0b55712d440c
David Gonzalez <dgonzalez@fagorelectronica.es>
2024-11-21 18:18:24 +0100

    plande pruebas david

---
+++ AndroidProject/app/src/androidTest/java/es/unican/gasolineras/activities/details/detailsCasoExitoUITest.java
@@ -0,0 +1,5 @@
+package es.unican.gasolineras.activities.details;
+
+public class detailsCasoExitoUITest {
+    //En este caso realizariamos las pruebas de interfaz de la vista details y comprobariamos los 5 casos de uso
+}
---
+++ AndroidProject/app/src/test/java/es/unican/gasolineras/activities/details/detailsTest.java
@@ -0,0 +1,7 @@
+package es.unican.gasolineras.activities.details;
+
+public class detailsTest {
+
+    //en este caso realizariamos las pruebas unitarias y de intergación del metodo load() de la clase detailsPresenter
+
+}
